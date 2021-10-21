33399e5879d81fafc8d5e57476a0cf382747791f
KP <kdp119@scarletmail.rutgers.edu>
2021-10-20 21:04:37 -0400

    ClassExit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController
+{
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    var eggTimes = ["Hard" : 10, "Medium" : 7, "Soft" : 4]
+    //Dictionary
+    
+    var startTime = 0
+    var totalTime = 0
+    var timer = Timer()
+    
+    
+    
+    @IBAction func clickMe(_ sender: UIButton) {
+        
+        timer.invalidate()
+        progressView.progress = 0.0
+        startTime = 0
+        
+        
+        let hardness =  sender.currentTitle!
+        
+       print(hardness)
+        
+       totalTime = eggTimes[hardness]!
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+        
     }
-
+    
+    @objc func updateCounter() {
+       
+            if startTime < totalTime  {
+                
+               
+                startTime += 1
+                progressView.progress = Float(startTime)/Float(totalTime)
+                
+            }
+            
+            else {
+                
+                outputLabel.text = "Eggs are ready!"
+                timer.invalidate()
+            }
+    
 
 }
 
+}
